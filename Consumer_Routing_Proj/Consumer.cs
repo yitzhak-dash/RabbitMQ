0108dcdb74c6b0104f8f8b269018dc8a1e30a38c
Yitzhak <yitzhak@razorgrip.com>
2016-04-27 17:33:53 +0200

    routing

---
+++ Consumer_Routing_Proj/Consumer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer_Routing_Proj
+{
+    public class Consumer
+    {
+        public void Run(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("direct_logs", "direct");
+
+                // we create a non-durable, exclusive, autodelete queue with a generated name
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]", Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "direct_logs",
+                        // The meaning of a binding key(routingKey) depends on the exchange type. 
+                                      routingKey: severity);
+                }
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [*] Received '{0}':'{1}'", routingKey, message);
+                };
+                channel.BasicConsume(queue: queueName, noAck: true, consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer_Routing_Proj/Producer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer_Routing_Proj
+{
+    /// <summary>
+    /// We will use a direct exchange instead.
+    /// The routing algorithm behind a direct exchange is simple - a message goes to the queues whose binding key exactly matches the routing key of the message.
+    /// </summary>
+    public class Producer
+    {
+        public void Run(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("direct_logs", "direct");
+
+                    var severity = GetSeverity(args);
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    // routing key is severity
+                    channel.BasicPublish(exchange: "direct_logs", routingKey: severity, basicProperties: null, body: body);
+                    Console.WriteLine("[*] Sent '{0}':'{1}'", severity, message);
+                }
+            }
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+        }
+
+        private static string GetSeverity(string[] args)
+        {
+            return args.Length > 0 ? args[0] : "info";
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 1
+                          ? string.Join(" ", args.Skip(1).ToArray())
+                          : "Hello World!";
+        }
+    }
+}
